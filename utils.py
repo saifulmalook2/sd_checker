b77a5908f29335e1ebaad5f79cb0e470e94d7584
Saif Ul Malook <saifulmalook16@gmail.com>
2024-09-26 18:06:40 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -23,7 +23,7 @@ async def check_company(html_text, company_name):
             {"role": "user", "content": f"Check if the company mentioned in the following page content matches '{company_name}'. If the company name mentioned does not match, return a list of JSON objects, each containing the incorrect company name and the sentence it is mentioned in (The sentence should be plain text, not HTML). Format the response as mistakes : [{{'incorrect_name': '...', 'sentence': '...'}}, {{'incorrect_name': '...', 'sentence': '...'}}]. Find all the incorrect names and append the JSON to the list. Page content: {html_text}"}
             ],
         )
-
+        print(response)
         response_text = response.choices[0].message.content.strip()
         filtered_response = json.loads(response_text)
 
